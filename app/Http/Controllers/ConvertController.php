2c42c56ae74c8775b16c28f11e852a00055034cc
cnxh <do.not.reply@foxmail.com>
2016-03-08 21:48:57 +0800

    Delete unsupported encoding in base64

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -15,8 +15,7 @@ class ConvertController extends Controller
         'ISO-8859-1', 'ISO-8859-2', 'ISO-8859-3', 'ISO-8859-4',
         'ISO-8859-5', 'ISO-8859-6', 'ISO-8859-7', 'ISO-8859-8', 'ISO-8859-9',
         'ISO-8859-10', 'ISO-8859-13', 'ISO-8859-14', 'ISO-8859-15', 'ISO-8859-16',
-        'CP51932', 'CP932', 'CP936', 'CP866', 'CP850',
-        'CP50220', 'CP50221', 'CP50222', ];
+        'CP932', 'CP936', 'CP866', 'CP850'];
 
     public function getUrlencode()
     {
